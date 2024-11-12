9dc6f3cca2c14293e0791cfd4517aae1b97bd34f
Vaishnav88sk <vaishnavsk8804@gmail.com>
2024-11-12 23:16:21 +0530

    Code updated

--- MongoDB_Java_Connectivity/MongoJava.java
+++ MongoDB_Java_Connectivity/MongoJava.java
@@ -1,9 +1,9 @@
 import com.mongodb.*;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.MongoClient;
+// import com.mongodb.BasicDBObject;
+// import com.mongodb.DB;
+// import com.mongodb.DBCollection;
+// import com.mongodb.DBCursor;
+// import com.mongodb.MongoClient;
 import java.util.Scanner;
 
 public class MongoJava {
